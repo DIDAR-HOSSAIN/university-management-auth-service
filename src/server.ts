f3690943c686ab486ea60cfda5d782f86248b301
DIDAR-HOSSAIN <didarhossain018@gmail.com>
2023-05-29 15:35:11 +0600

    Project Setup updated

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
 import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
 
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    try{
+        await mongoose.connect(config.database_url as string);
+        console.log(`Database Connected Successfully`);
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+        app.listen(config.port, () => {
+            console.log(`UM Application listening on port ${config.port}`);
+        });
+
+    }catch(err){
+        console.log("Failed to Connect Database", err);
+    }
 }
+
+main();
