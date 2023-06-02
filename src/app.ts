6afb76bec9eae05641d0eee0afb61e5f10efbfa8
DIDAR-HOSSAIN <didarhossain018@gmail.com>
2023-06-02 19:52:02 +0600

    skipping hooks

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express, { Application } from "express"
-
+import express, { Application, Request, Response } from "express"
+import cors from "cors";
 
 const app:Application = express();
-// app.use(cors());
+                  app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+//parser
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', (req:Request, res:any) => {
+    res.send('Working Successfully')
 })
 
 export default app;
