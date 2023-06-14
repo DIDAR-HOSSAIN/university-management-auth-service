aa1476f7f6cfa839e5fac8def082d994365a4a12
DIDAR-HOSSAIN <didarhossain018@gmail.com>
2023-06-14 16:07:30 +0600

    added pagination

---
+++ src/helpers/paginationHelper.ts
@@ -0,0 +1,26 @@
+type IOptions = {
+  page?: number;
+  limit?: number;
+};
+
+type IOptionsResult = {
+  page?: number;
+  limit?: number;
+  skip?: number;
+};
+
+const calculatePagination = (options: IOptions): IOptionsResult => {
+  const page = Number(options.page || 1);
+  const limit = Number(options.limit || 10);
+  const skip = (page - 1) * limit;
+
+  return {
+    page,
+    limit,
+    skip,
+  };
+};
+
+export const PaginationHelpers = {
+  calculatePagination,
+};
